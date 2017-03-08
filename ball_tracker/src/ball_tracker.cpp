8273faf06f428c6ceff5645a51b12f7963f94199
Luke Newmeyer <luke.newmeyer@byu.edu>
2017-03-08 09:23:18 -0700

    Files added to begin Assignment 4

---
+++ ball_tracker/src/ball_tracker.cpp
@@ -0,0 +1,15 @@
+// EcEn 631 - Robotic Vision
+// Assignment 4 - 3D Reconstruction and Trajectory Estimation
+// Luke Newmeyer
+
+#include <iostream>
+
+// Namespaces used commonly
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
