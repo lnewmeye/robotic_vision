7a5edd188d714fb8143797bd974625916fab9b53
Luke Newmeyer <luke.newmeyer@byu.edu>
2017-03-14 18:39:27 -0600

    Working Motion Field project (assignmetn 5). Submitted

--- motion_field/doc/motion_field.md
+++ motion_field/doc/motion_field.md
@@ -5,201 +5,36 @@
 
 The following are the images from using the Lucas-Kanade method for optical follow. From these images you can see that images with fewer skips in frames result in greater resolution but less clarity in the direction of the gradient. There is a trade-off between these two.
 
-### Single Frame Flow
-
-![Single Flow 1](../output/flow1_00.jpg)
-
-![Single Flow 2](../output/flow1_01.jpg)
-
-![Single Flow 3](../output/flow1_02.jpg)
-
-![Single Flow 4](../output/flow1_03.jpg)
-
-![Single Flow 5](../output/flow1_04.jpg)
-
-![Single Flow 6](../output/flow1_05.jpg)
-
-![Single Flow 7](../output/flow1_06.jpg)
-
-![Single Flow 8](../output/flow1_07.jpg)
-
-![Single Flow 9](../output/flow1_08.jpg)
-
-![Single Flow 10](../output/flow1_09.jpg)
-
-![Single Flow 11](../output/flow1_10.jpg)
-
-![Single Flow 12](../output/flow1_11.jpg)
-
-![Single Flow 13](../output/flow1_12.jpg)
-
-![Single Flow 14](../output/flow1_13.jpg)
-
-![Single Flow 15](../output/flow1_14.jpg)
-
-![Single Flow 16](../output/flow1_15.jpg)
-
-### Double Frame Flow
-
-![Double Flow 1](../output/flow2_00.jpg)
-
-![Double Flow 2](../output/flow2_01.jpg)
-
-![Double Flow 3](../output/flow2_02.jpg)
-
-![Double Flow 4](../output/flow2_03.jpg)
-
-![Double Flow 5](../output/flow2_04.jpg)
-
-![Double Flow 6](../output/flow2_05.jpg)
-
-![Double Flow 7](../output/flow2_06.jpg)
-
-![Double Flow 8](../output/flow2_07.jpg)
-
-![Double Flow 9](../output/flow2_08.jpg)
-
-![Double Flow 10](../output/flow2_09.jpg)
-
-![Double Flow 11](../output/flow2_10.jpg)
-
-![Double Flow 12](../output/flow2_11.jpg)
-
-![Double Flow 13](../output/flow2_12.jpg)
-
-![Double Flow 14](../output/flow2_13.jpg)
-
-![Double Flow 15](../output/flow2_14.jpg)
-
-### Triple Frame Flow
-
-![Triple Flow 1](../output/flow3_00.jpg)
-
-![Triple Flow 2](../output/flow3_01.jpg)
-
-![Triple Flow 3](../output/flow3_02.jpg)
-
-![Triple Flow 4](../output/flow3_03.jpg)
-
-![Triple Flow 5](../output/flow3_04.jpg)
-
-![Triple Flow 6](../output/flow3_05.jpg)
-
-![Triple Flow 7](../output/flow3_06.jpg)
-
-![Triple Flow 8](../output/flow3_07.jpg)
-
-![Triple Flow 9](../output/flow3_08.jpg)
-
-![Triple Flow 10](../output/flow3_09.jpg)
-
-![Triple Flow 11](../output/flow3_10.jpg)
-
-![Triple Flow 12](../output/flow3_11.jpg)
-
-![Triple Flow 13](../output/flow3_12.jpg)
-
-![Triple Flow 14](../output/flow3_13.jpg)
-
-
 ## Task 2 - Feature Matching
 
-### Single Frame Flow
-
-![Single Flow 1](../output/match1_00.jpg)
-
-![Single Flow 2](../output/match1_01.jpg)
-
-![Single Flow 3](../output/match1_02.jpg)
-
-![Single Flow 4](../output/match1_03.jpg)
-
-![Single Flow 5](../output/match1_04.jpg)
-
-![Single Flow 6](../output/match1_05.jpg)
-
-![Single Flow 7](../output/match1_06.jpg)
-
-![Single Flow 8](../output/match1_07.jpg)
-
-![Single Flow 9](../output/match1_08.jpg)
-
-![Single Flow 10](../output/match1_09.jpg)
-
-![Single Flow 11](../output/match1_10.jpg)
-
-![Single Flow 12](../output/match1_11.jpg)
-
-![Single Flow 13](../output/match1_12.jpg)
-
-![Single Flow 14](../output/match1_13.jpg)
-
-![Single Flow 15](../output/match1_14.jpg)
-
-![Single Flow 16](../output/match1_15.jpg)
-
-### Double Frame Flow
-
-![Double Flow 1](../output/match2_00.jpg)
-
-![Double Flow 2](../output/match2_01.jpg)
-
-![Double Flow 3](../output/match2_02.jpg)
-
-![Double Flow 4](../output/match2_03.jpg)
-
-![Double Flow 5](../output/match2_04.jpg)
-
-![Double Flow 6](../output/match2_05.jpg)
-
-![Double Flow 7](../output/match2_06.jpg)
-
-![Double Flow 8](../output/match2_07.jpg)
-
-![Double Flow 9](../output/match2_08.jpg)
-
-![Double Flow 10](../output/match2_09.jpg)
-
-![Double Flow 11](../output/match2_10.jpg)
-
-![Double Flow 12](../output/match2_11.jpg)
-
-![Double Flow 13](../output/match2_12.jpg)
-
-![Double Flow 14](../output/match2_13.jpg)
-
-![Double Flow 15](../output/match2_14.jpg)
-
-### Triple Frame Flow
-
-![Triple Flow 1](../output/match3_00.jpg)
-
-![Triple Flow 2](../output/match3_01.jpg)
-
-![Triple Flow 3](../output/match3_02.jpg)
-
-![Triple Flow 4](../output/match3_03.jpg)
-
-![Triple Flow 5](../output/match3_04.jpg)
-
-![Triple Flow 6](../output/match3_05.jpg)
-
-![Triple Flow 7](../output/match3_06.jpg)
-
-![Triple Flow 8](../output/match3_07.jpg)
-
-![Triple Flow 9](../output/match3_08.jpg)
-
-![Triple Flow 10](../output/match3_09.jpg)
-
-![Triple Flow 11](../output/match3_10.jpg)
-
-![Triple Flow 12](../output/match3_11.jpg)
-
-![Triple Flow 13](../output/match3_12.jpg)
-
-![Triple Flow 14](../output/match3_13.jpg)
-
+The results from my feature matching algorithm seem to be less accurate than the Lucas-Kanade algorithm. Perhaps some more tuning of the parameters would improve this. I played with quite a few of the parameters but didn't have much success.
 
 ## Task 3 - Multi-Frame Feature Tracking
+
+The Multi-frame feature tracking works quite well. Some minor challenges occur when ambiguity exists in the image. In general, however, a clear direction of movement can be seen.
+
+### Parallel Cube
+
+![Parallel Cube 1](../output/parallel_cube/parallel_cube1.jpg)
+
+![Parallel Cube 5](../output/parallel_cube/parallel_cube5.jpg)
+
+### Parallel Real
+
+![Parallel Real 1](../output/parallel_real/parallel_real1.jpg)
+
+![Parallel Real 5](../output/parallel_real/parallel_real5.jpg)
+
+### Turned Cube
+
+![Turned Cube 1](../output/turned_cube/turned_cube1.jpg)
+
+![Turned Cube 5](../output/turned_cube/turned_cube5.jpg)
+
+### Parallel Real
+
+![Turned Real 1](../output/turned_real/turned_real1.jpg)
+
+![Turned Real 5](../output/turned_real/turned_real5.jpg)
+
 
--- motion_field/src/motion_field.cpp
+++ motion_field/src/motion_field.cpp
@@ -9,6 +9,7 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/video/tracking.hpp>
 
 // Standard namespaces used by default
@@ -36,10 +37,15 @@ using cv::Point2f;
 #define DISPLAY_WINDOW_1 "Display Window 1"
 #define DISPLAY_WINDOW_2 "Display Window 2"
 #define DISPLAY_WINDOW_3 "Display Window 3"
-#define DISPLAY_TIME_FAST 10
+#define DISPLAY_TIME_FAST 100
+#define DISPLAY_TIME_SLOW 0
 
 // Parameters for image input
 #define IMAGE_OPTICAL_FLOW "data/optical_flow/O%01d.jpg"
+#define IMAGE_TRACKING_1 "data/parallel_cube/ParallelCube%01d.jpg"
+#define IMAGE_TRACKING_2 "data/parallel_real/ParallelReal%01d.jpg"
+#define IMAGE_TRACKING_3 "data/turned_cube/TurnCube%01d.jpg"
+#define IMAGE_TRACKING_4 "data/turned_real/TurnReal%01d.jpg"
 
 // Parameters for image output
 #define IMAGE_OUTPUT_FOLDER "output/"
@@ -58,17 +64,25 @@ using cv::Point2f;
 #define OPTICAL_FLOW_LEVEL 3
 
 // Parameters for template matching task
-#define MATCH_SELECTION_WIDTH 40
-#define MATCH_SELECTION_HEIGHT 40
-#define MATCH_BLOCK_WIDTH 20
-#define MATCH_BLOCK_HEIGHT 20
+#define MATCH_SELECTION_WIDTH 60
+#define MATCH_SELECTION_HEIGHT 60
+#define MATCH_BLOCK_WIDTH 30
+#define MATCH_BLOCK_HEIGHT 30
+
+// Parameters for tracking task
+#define TRACK_CORNER_MAX 1000
+#define TRACK_CORNER_QUALITY 0.01
+#define TRACK_CORNER_DISTANCE 2
 
 // Function declarations
 void drawCorners(Mat& image, vector<Point2f> corners);
+void drawFlow(Mat& image, vector<Point2f> corners, vector<Point2f> flow);
 void drawFlow(Mat& image, vector<Point2f> corners, vector<Point2f> flow,
 		vector<unsigned char> status);
 string generateFilename(string folder, string prefix, int number, string type);
 vector<Point2f> opticalMatching(Mat& initial, Mat& image, vector<Point2f> corners);
+vector<Point2f> opticalMatchingFine(Mat& initial, Mat& image, vector<Point2f> corners);
+void trackMultiFrame(VideoCapture sequence);
 
 int main()
 {
@@ -102,7 +116,7 @@ int main()
 
 	// Create variables for optical flow loop
 	vector<Point2f> corners;
-	Mat display1, display2, display3;
+	Mat display, display1, display2, display3;
 	vector<Point2f> flow1, flow2, flow3;
 	vector<unsigned char> status1, status2, status3;
 	vector<float> error;
@@ -267,6 +281,42 @@ int main()
 		cv::cvtColor(image, image3, CV_BGR2GRAY);
 	}
 
+	// Create VideoCaputre to read image tracking data (1)
+	VideoCapture image_sequence(IMAGE_TRACKING_1);
+	if (!image_sequence.isOpened()) {
+		cout << "Error: couldn't open image sequence" << endl;
+		return -1;
+	}
+
+	trackMultiFrame(image_sequence);
+
+	// Open next image tracking sequence
+	image_sequence.open(IMAGE_TRACKING_2);
+	if (!image_sequence.isOpened()) {
+		cout << "Error: couldn't open image sequence" << endl;
+		return -1;
+	}
+
+	trackMultiFrame(image_sequence);
+
+	// Open next image tracking sequence
+	image_sequence.open(IMAGE_TRACKING_3);
+	if (!image_sequence.isOpened()) {
+		cout << "Error: couldn't open image sequence" << endl;
+		return -1;
+	}
+
+	trackMultiFrame(image_sequence);
+
+	// Open next image tracking sequence
+	image_sequence.open(IMAGE_TRACKING_4);
+	if (!image_sequence.isOpened()) {
+		cout << "Error: couldn't open image sequence" << endl;
+		return -1;
+	}
+
+	trackMultiFrame(image_sequence);
+
 	return 0;
 }
 
@@ -296,8 +346,26 @@ void drawFlow(Mat& image, vector<Point2f> corners, vector<Point2f> flow,
 
 			// Draw circle and flow vector
 			cv::circle(image, corner, 4, Scalar(0,255,0), -1, 8, 0);
-			cv::arrowedLine(image, tip, corner, Scalar(0,0,255));
+			cv::arrowedLine(image, corner, tip, Scalar(0,0,255));
 		}
+	}
+}
+
+void drawFlow(Mat& image, vector<Point2f> corners, vector<Point2f> flow)
+{
+	// Create variables for loop
+	Point2f corner, tip;
+
+	// Iterate through vectors and plot if status shows flow to be found
+	for (unsigned i = 0; i < corners.size(); i++) {
+
+		// Get corner and flow values
+		corner = corners[i];
+		tip = flow[i];
+
+		// Draw circle and flow vector
+		cv::circle(image, corner, 4, Scalar(0,255,0), -1, 8, 0);
+		cv::arrowedLine(image, corner, tip, Scalar(0,0,255));
 	}
 }
 
@@ -332,6 +400,10 @@ vector<Point2f> opticalMatching(Mat& initial, Mat& image,
 
 	// Iterate through corners and find best fit flow
 	for (Point2f corner : corners) {
+
+		// Replace corner if it comes back negative
+		if (corner.x < 1 || corner.y < 1)
+			corner = Point(1,1);
 
 		// Find positions for defining selection
 		x1 = corner.x - MATCH_SELECTION_WIDTH/2;
@@ -385,4 +457,177 @@ vector<Point2f> opticalMatching(Mat& initial, Mat& image,
 	}
 
 	return matches;
+}
+
+
+vector<Point2f> opticalMatchingFine(Mat& initial, Mat& image,
+		vector<Point2f> corners)
+{
+	// Create variables for loop
+	int x1, x2, y1, y2;
+	Mat selection, block, result;
+	Rect selection_rect, block_rect;
+	int image_width = initial.cols;
+	int image_height = initial.rows;
+	vector<Point2f> matches;
+	Point max_location;
+	Point2f match;
+
+	// Iterate through corners and find best fit flow
+	for (Point2f corner : corners) {
+
+		// Replace corner if it comes back negative
+		if (corner.x < 1 || corner.y < 1)
+			corner = Point(1,1);
+
+		// Find positions for defining selection
+		x1 = corner.x - MATCH_SELECTION_WIDTH/2;
+		y1 = corner.y - MATCH_SELECTION_HEIGHT/2;
+		x2 = x1 + MATCH_SELECTION_WIDTH;
+		y2 = y1 + MATCH_SELECTION_HEIGHT;
+
+		// Correct positions of wrong
+		if (x1 <= 0)
+			x1 = 1;
+		if (y1 <= 0)
+			y1 = 1;
+		if (x2 >= image_width)
+			x2 = image_width-1;
+		if (y2 >= image_height)
+			y2 = image_height-1;
+
+		// Create rects defining regions for matching
+		selection_rect = Rect(Point(x1,y1), Point(x2,y2));
+
+		// Find positions for defining block
+		x1 = corner.x - MATCH_BLOCK_WIDTH/2;
+		y1 = corner.y - MATCH_BLOCK_HEIGHT/2;
+		x2 = x1 + MATCH_BLOCK_WIDTH;
+		y2 = y1 + MATCH_BLOCK_HEIGHT;
+
+		// Correct positions of wrong
+		if (x1 <= 0)
+			x1 = 1;
+		if (y1 <= 0)
+			y1 = 1;
+		if (x2 >= image_width)
+			x2 = image_width-1;
+		if (y2 >= image_height)
+			y2 = image_height-1;
+
+		// Set rect defining block
+		block_rect = Rect(Point(x1,y1), Point(x2,y2));
+
+		// Select regions of initial and match images
+		selection = image(selection_rect);
+		block = initial(block_rect);
+
+		// Match block in selection
+		cv::matchTemplate(selection, block, result, cv::TM_CCOEFF);
+		normalize(result, result, 0, 1, cv::NORM_MINMAX, -1, Mat());
+		cv::minMaxLoc(result, NULL, NULL, NULL, &max_location);
+		match.x = max_location.x + corner.x - MATCH_BLOCK_WIDTH/2;
+		match.y = max_location.y + corner.y - MATCH_BLOCK_HEIGHT/2;
+		matches.push_back(match);
+	}
+
+	// Resolve corner find grain location
+	Size subpix_size(12,12);
+	Size zero_zone(-1,-1);
+	cv::TermCriteria subpix_criteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 
+			40, 0.001);
+	cv::cornerSubPix(image, matches, subpix_size, zero_zone, subpix_criteria);
+
+	return matches;
+}
+
+void trackMultiFrame(VideoCapture sequence)
+{
+	// Setup variables for process
+	Mat image, image1, image2, display;
+	vector<Point2f> corners, flow_corners;
+	vector<vector<Point2f>> tracked_corners;
+	vector<Mat> previous_images;
+	vector<uchar> mask;
+	uchar match;
+
+	// Ignore first few frames (we want frames 10 through 15)
+	for (int i = 1; i < 10; i++) {
+		sequence >> image;
+	}
+
+	// Find 1000 points in initial image
+	sequence >> image;
+	cv::cvtColor(image, image1, CV_BGR2GRAY);
+	cv::goodFeaturesToTrack(image1, corners, TRACK_CORNER_MAX,
+			TRACK_CORNER_QUALITY, TRACK_CORNER_DISTANCE); 
+
+	// Refine points
+	Size subpix_size(5,5);
+	Size zero_zone(-1,-1);
+	cv::TermCriteria subpix_criteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 
+			40, 0.001);
+	cv::cornerSubPix(image1, corners, subpix_size, zero_zone,
+			subpix_criteria);
+
+	// Push corners to tracked vector and image
+	tracked_corners.push_back(corners);
+	previous_images.push_back(image1.clone());
+
+	// Track points through frames 10 through 15
+	for (int i = 10; i < 15; i++) {
+
+		// Get new images
+		image2 = image1.clone();
+		sequence >> image;
+		cv::cvtColor(image, image1, CV_BGR2GRAY);
+		previous_images.push_back(image1.clone());
+
+		// Match points between images
+		flow_corners = opticalMatchingFine(image2, image1, corners);
+		tracked_corners.push_back(flow_corners);
+		cv::findFundamentalMat(corners, flow_corners, cv::FM_RANSAC,
+				3, 0.99, mask);
+
+		// Remove corners not matching between imges
+		for (int j = mask.size(); j > 0; j--) {
+
+			// Pop off from mask
+			match = mask.back();
+			mask.pop_back();
+
+			// Remove item off of not a match
+			if (!match) {
+				for (unsigned k = 0; k < tracked_corners.size(); k++) {
+					tracked_corners[k].erase(tracked_corners[k].begin()+j-1);
+				}
+			}
+		}
+
+		// Temporarily display image
+		flow_corners = tracked_corners.back();
+		corners = tracked_corners.at(tracked_corners.size()-2);
+		//cv::cvtColor(image1, display, CV_GRAY2BGR);
+		//drawFlow(display, corners, flow_corners);
+		//cv::imshow(DISPLAY_WINDOW_1, display);
+		//cv::waitKey(0);
+
+		corners = flow_corners;
+	}
+
+	// Display image sequence
+	for (unsigned i = 1; i < previous_images.size(); i++) {
+
+		// Get corners and image to be displayed
+		corners = tracked_corners[i-1];
+		flow_corners = tracked_corners[i];
+		cv::cvtColor(previous_images[i-1], display, CV_GRAY2BGR);
+
+		// Draw image
+		drawFlow(display, flow_corners, corners);
+		cv::imshow(DISPLAY_WINDOW_3, display);
+		cv::imwrite("output/tmp/tmp" + 
+				std::to_string(i) + ".jpg", display);
+		cv::waitKey(DISPLAY_TIME_SLOW);
+	}
 }
