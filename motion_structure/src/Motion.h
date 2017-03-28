69507858afc5a28b9306eaef37d6a1d17a6212d1
Luke Newmeyer <luke.newmeyer@byu.edu>
2017-03-28 12:56:01 -0600

    Progress but facing bugs in code

--- motion_structure/src/Motion.h
+++ motion_structure/src/Motion.h
@@ -14,35 +14,56 @@
 #define MOTION_CORNER_DISTANCE 2
 
 // Parameters for template matching task
-#define MATCH_SELECTION_WIDTH 60
-#define MATCH_SELECTION_HEIGHT 60
-#define MATCH_BLOCK_WIDTH 30
-#define MATCH_BLOCK_HEIGHT 30
+#define MATCH_SELECTION_WIDTH 120
+#define MATCH_SELECTION_HEIGHT 120
+#define MATCH_BLOCK_WIDTH 60
+#define MATCH_BLOCK_HEIGHT 60
 
-// Guessed parameters (for task 1)
+// Guessed parameters (for Task 1)
 #define IMAGE_CENTER_GUESS_X 320
 #define IMAGE_CENTER_GUESS_Y 240
 #define IMAGE_FOCAL_GUESS 1145
 #define IMAGE_DISTORTION_GUESS \
-	-0.2575507934862325, \
-	 0.04884197280891184, \
-	-0.001409751152446753, \
-	-0.001543707631160005, \
-	 0.9076529637760357
+		-0.2575507934862325, \
+		 0.04884197280891184, \
+		-0.001409751152446753, \
+		-0.001543707631160005, \
+		 0.9076529637760357
+
+// Known parameters (for Task 2)
+#define IMAGE_INTRINSIC_KNOWN \
+		825.0900600547,    0.0000000000,  331.6538103208, \
+		0.0000000000,  824.2672147458,  252.9284287373, \
+		0.0000000000,    0.0000000000,    1.0000000000
+#define IMAGE_DISTORTION_KNOWN \
+		-0.2380769337, \
+		0.0931325835, \
+		0.0003242537, \
+		-0.0021901930, \
+		0.4641735616
 
 // Input image parameters
 #define IMAGE_WIDTH 640
 #define IMAGE_HEIGHT 480
+
+// Drawing parameters
+#define LINE_SPACING 20
 
 class Motion
 {
 	public:
 		// Object operations
 		Motion();
+		void reset();
 		void setInitial(cv::Mat image);
 		void addFrame(cv::Mat image);
 		cv::Mat averageIntrinsic();
 		void rectifyImage(cv::Mat& display, cv::Mat& display2);
+		void findEssential();
+		cv::Mat getFundamental();
+		cv::Mat getEssential();
+		cv::Mat getRotation();
+		cv::Mat getTranslation();
 
 		// Object get functions
 		std::vector<cv::Mat> getMotionImages();
@@ -53,6 +74,7 @@ class Motion
 				cv::Mat& image, std::vector<cv::Point2f> corners);
 		void drawMotion(cv::Mat& image, std::vector<cv::Point2f> corners, 
 			std::vector<cv::Point2f> flow);
+		void drawHorizontalLines(cv::Mat& image);
 
 		// Private variables
 		std::vector<std::vector<cv::Point2f>> tracked_corners;
--- motion_structure/src/motion_structure.cpp
+++ motion_structure/src/motion_structure.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <string>
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
@@ -15,25 +17,18 @@
 // Standard namespaces used by default
 using std::cout;
 using std::endl;
+using std::queue;
 using std::vector;
+using std::string;
 
 // OpenCV namespaces used by default
 using cv::VideoCapture;
 using cv::Mat;
 
-// DEBUG option (comment out when not in use)
-//#define DEBUG
-#ifdef DEBUG
-#define DISPLAY_DEBUG "Debug Display"
-#endif
-
-// Application options
-#define TASK 1 // Options: 1, 2, or 3
-
 // Parameters for display window
 #define DISPLAY_WINDOW_1 "Display Window 1"
 #define DISPLAY_WINDOW_2 "Display Window 2"
-#define DISPLAY_TIME_FAST 1
+#define DISPLAY_TIME_FAST 0
 #define DISPLAY_TIME_SLOW 0
 
 // Parameters for image input
@@ -43,20 +38,14 @@ using cv::Mat;
 #define IMAGE_TRACKING_4 "data/turned_real/TurnReal%01d.jpg"
 
 // Parameters for image output
-//#define IMAGE_OUTPUT_FOLDER "output/"
-//#define IMAGE_OUTPUT_TYPE ".jpg"
-//#define IMAGE_OUTPUT_FLOW1 "flow1_"
-//#define IMAGE_OUTPUT_FLOW2 "flow2_"
-//#define IMAGE_OUTPUT_FLOW3 "flow3_"
-//#define IMAGE_OUTPUT_MATCH1 "match1_"
-//#define IMAGE_OUTPUT_MATCH2 "match2_"
-//#define IMAGE_OUTPUT_MATCH3 "match3_"
-
-// Parameters for optical flow task
-//#define OPTICAL_CORNER_MAX 500
-//#define OPTICAL_CORNER_QUALITY 0.01
-//#define OPTICAL_CORNER_DISTANCE 5
-//#define OPTICAL_FLOW_LEVEL 3
+#define OUTPUT_RECTIFY_FIRST_1 "output/parallel_cube_first.jpg"
+#define OUTPUT_RECTIFY_LAST_1 "output/parallel_cube_last.jpg"
+#define OUTPUT_RECTIFY_FIRST_2 "output/parallel_real_first.jpg"
+#define OUTPUT_RECTIFY_LAST_2 "output/parallel_real_last.jpg"
+#define OUTPUT_RECTIFY_FIRST_3 "output/turned_cube_first.jpg"
+#define OUTPUT_RECTIFY_LAST_3 "output/turned_cube_last.jpg"
+#define OUTPUT_RECTIFY_FIRST_4 "output/turned_real_first.jpg"
+#define OUTPUT_RECTIFY_LAST_4 "output/turned_real_last.jpg"
 
 int main()
 {
@@ -65,66 +54,94 @@ int main()
 	Mat image, display, display1, display2;
 	int keypress;
 	cv::namedWindow(DISPLAY_WINDOW_1, CV_WINDOW_AUTOSIZE);
+	cv::namedWindow(DISPLAY_WINDOW_2, CV_WINDOW_AUTOSIZE);
 	Motion motion;
 
-	// Create debug window (if defined)
-#ifdef DEBUG
-	cv::namedWindow(DISPLAY_DEBUG, CV_WINDOW_NORMAL);
-#endif
+	// Load vector with image sequence strings
+	vector<string> image_names;
+	image_names.push_back(IMAGE_TRACKING_1);
+	image_names.push_back(IMAGE_TRACKING_2);
+	image_names.push_back(IMAGE_TRACKING_3);
+	image_names.push_back(IMAGE_TRACKING_4);
+	queue<string> output_first;
+	output_first.push(OUTPUT_RECTIFY_FIRST_1);
+	output_first.push(OUTPUT_RECTIFY_FIRST_2);
+	output_first.push(OUTPUT_RECTIFY_FIRST_3);
+	output_first.push(OUTPUT_RECTIFY_FIRST_4);
+	queue<string> output_last;
+	output_last.push(OUTPUT_RECTIFY_LAST_1);
+	output_last.push(OUTPUT_RECTIFY_LAST_2);
+	output_last.push(OUTPUT_RECTIFY_LAST_3);
+	output_last.push(OUTPUT_RECTIFY_LAST_4);
 
-	// Open image sequence
-	image_sequence.open(IMAGE_TRACKING_1);
+	// Iterate through image names and operate on sequence
+	for (string image_name : image_names) {
 
-	// Read in first image and set as inital in Motion object
-	image_sequence >> image;
-	cv::cvtColor(image, image, CV_BGR2GRAY);
-	motion.setInitial(image);
+		// Open image sequence
+		cout << "Processing sequence: " << image_name << endl;
+		image_sequence.open(image_name);
 
-	// Read in next image and begin loop
-	image_sequence >> image;
-	keypress = 0;
-
-	// Loop while images available
-	while(image.data && keypress != 'q') {
-
-		// Convert image color
-		cv::cvtColor(image, image, CV_BGR2GRAY);
-
-		// Execute processing according to selected task
-		#if TASK == 1
-		motion.addFrame(image);
-		display = image.clone();
-		#elif TASK == 2
-		display = image.clone();
-		#elif TASK == 3
-		display = image.clone();
-		#endif
-
-		// Display, wait, and load next image
-		cv::imshow(DISPLAY_WINDOW_1, display);
-		keypress = cv::waitKey(DISPLAY_TIME_FAST);
+		// Read in first image and set as inital in Motion object
 		image_sequence >> image;
+		cv::cvtColor(image, image, CV_BGR2GRAY);
+		motion.setInitial(image);
+
+		// Read in next image and begin loop
+		image_sequence >> image;
+		keypress = 0;
+
+		// Loop while images available
+		while(image.data && keypress != 'q') {
+
+			// Convert image color
+			cv::cvtColor(image, image, CV_BGR2GRAY);
+			motion.addFrame(image);
+			display = image;
+
+			// Display, wait, and load next image
+			cv::imshow(DISPLAY_WINDOW_1, display);
+			keypress = cv::waitKey(DISPLAY_TIME_FAST);
+			image_sequence >> image;
+		}
+
+		// Get images with motion displayed and display
+		vector<Mat> display_images = motion.getMotionImages();
+		for (Mat display_image : display_images) {
+			cv::imshow(DISPLAY_WINDOW_1, display_image);
+			keypress = cv::waitKey(DISPLAY_TIME_SLOW);
+			if (keypress == 'q')
+				break;
+		}
+
+		// Rectify image based on guess of parameters (Task 1)
+		motion.rectifyImage(display1, display2);
+		cv::imshow (DISPLAY_WINDOW_1, display1);
+		cv::imshow (DISPLAY_WINDOW_2, display2);
+		cv::waitKey(DISPLAY_TIME_SLOW);
+		cv::imwrite(output_first.front(), display1);
+		cv::imwrite(output_last.front(), display2);
+		output_first.pop();
+		output_last.pop();
+
+		// Compute essential matrix (Task 2)
+		Mat F, E, R, T;
+		motion.findEssential();
+		F = motion.getFundamental();
+		E = motion.getEssential();
+		R = motion.getRotation();
+		T = motion.getTranslation();
+		cout << "F =" << endl << F << endl;
+		cout << "E =" << endl << E << endl;
+		cout << "R =" << endl << R << endl;
+		cout << "T =" << endl << T << endl;
+
+		// Find 3d info (Task 3)
+
+		// Reset for next sequence
+		motion.reset();
+		cout << endl;
+
 	}
-
-	// Get images with motion displayed and display
-	vector<Mat> display_images = motion.getMotionImages();
-	for (Mat display_image : display_images) {
-		cv::imshow(DISPLAY_WINDOW_1, display_image);
-		keypress = cv::waitKey(DISPLAY_TIME_SLOW);
-		if (keypress == 'q')
-			break;
-	}
-
-	// Output averaged intrinsic parameters
-	Mat F = motion.averageIntrinsic();
-	cout << "Averaged intrinsic parameters:" << endl;
-	cout << "F =" << endl << F << endl;
-
-	// Rectify image based on guess of parameters (task 1)
-	motion.rectifyImage(display1, display2);
-	cv::imshow (DISPLAY_WINDOW_1, display1);
-	cv::imshow (DISPLAY_WINDOW_2, display2);
-	cv::waitKey(DISPLAY_TIME_SLOW);
 
 	return 0;
 }
